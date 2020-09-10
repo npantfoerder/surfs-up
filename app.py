91dd8a33810e3c14939f9507b49ff721a7176776
Neele Pantfoerder <neelesd@gmail.com>
2020-09-09 22:27:03 -0700

    Added app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+# Install Flask
+#pip install flask
+from flask import Flask
+
+# Create a new Flask app instance
+app = Flask(__name__)
+
+# Create a Flask route
+@app.route('/')
+def hello_world():
+    return 'Hello world'
